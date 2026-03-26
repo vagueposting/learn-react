240b2f3bbcab33582c06f6afd8c425851d64c90d
vagueposting <vagueposted@gmail.com>
2026-03-26 18:46:05 +0800

    chore: cleanup on container

--- calculator/src/components/container.tsx
+++ calculator/src/components/container.tsx
@@ -2,10 +2,7 @@ import type { ContainerChildren } from "../types";
 
 export function Container({ children }: ContainerChildren) {
   return (
-    <div
-      className='flex justify-center items-center h-screen 
-            bg-linear-to-b from-teal-100 via-lime-100 to-amber-200'
-    >
+    <div className='flex justify-center items-center h-screen bg-linear-to-b from-teal-100 via-lime-100 to-amber-200'>
       {children}
     </div>
   );
