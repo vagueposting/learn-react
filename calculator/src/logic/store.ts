3d997bd2378c3b0b2fafeeb2efec529f47fd73ef
vagueposting <vagueposted@gmail.com>
2026-03-26 19:00:34 +0800

    refactor: changed store setup to use immer, added operation tracking

--- calculator/src/logic/store.ts
+++ calculator/src/logic/store.ts
@@ -1,24 +1,41 @@
-import { create } from 'zustand';
-import { calcFn } from './calcFn';
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { calcFn } from "./calcFn";
 
 interface CalculatorState {
-    a: number;
-    b: number;
-    result: number;
-    setA: (value: number) => void;
-    setB: (value: number) => void;
-    calculate: (operation: keyof typeof calcFn) => void;
+  a: number;
+  b: number;
+  operation: null | "add" | "subtract" | "multiply" | "divide";
+  result: number;
+  setA: (value: number) => void;
+  setB: (value: number) => void;
+  calculate: (operation: keyof typeof calcFn) => void;
 }
 
-export const useCalculatorStore = create<CalculatorState>((set, get) => ({
+export const useCalculatorStore = create<CalculatorState>()(
+  immer((set, get) => ({
     a: 0,
     b: 0,
+    operation: null,
     result: 0,
-    setA: (a) => set({ a }),
-    setB: (b) => set({ b }),
+    setA: (a) =>
+      set((state) => {
+        state.a = a;
+      }),
+    setB: (b) =>
+      set((state) => {
+        state.b = b;
+      }),
+    setOperation: (op: CalculatorState["operation"]) =>
+      set((state) => {
+        state.operation = op;
+      }),
     calculate: (operation) => {
-        const { a, b } = get();
-        const result = calcFn[operation](a, b);
-        set({ result })
-    }
-}));
+      const { a, b } = get();
+      const result = calcFn[operation](a, b);
+      set((state) => {
+        state.result = result;
+      });
+    },
+  })),
+);
