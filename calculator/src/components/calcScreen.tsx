3d83bac5adc03563994fb4a09db6617bbc278daf
vagueposting <vagueposted@gmail.com>
2026-03-27 20:02:06 +0800

    style: adjusted calculator screen style

--- calculator/src/components/calcScreen.tsx
+++ calculator/src/components/calcScreen.tsx
@@ -7,12 +7,45 @@ interface ScreenParam {
   operation?: CalculatorState["operation"];
 }
 
-export function CalculatorScreen({ extraClasses, value }: ScreenParam) {
+export function CalculatorScreen({
+  extraClasses,
+  value,
+  operation,
+}: ScreenParam) {
   const classes = cn(
+    "grid grid-cols-6 grid-rows-4 p-3 justify-start align-center",
     "rounded-md",
     "bg-gradient-to-br from-aqua-forest-400 via-aqua-forest-300 to-aqua-forest-400",
+    "text-3xl font-mono",
     extraClasses,
   );
+
+  let operationItem;
+
+  if (operation) {
+    let opSign;
+
+    switch (operation) {
+      case "add":
+        opSign = "+";
+        break;
+      case "subtract":
+        opSign = "-";
+        break;
+      case "multiply":
+        opSign = "×";
+        break;
+      case "divide":
+        opSign = "÷";
+        break;
+    }
+
+    operationItem = (
+      <div className='col-start-6 col-end-7 row-start-4 row-end-5 text-lg select-none justify-self-center self-center'>
+        {opSign}
+      </div>
+    );
+  }
 
   return (
     <div
@@ -21,7 +54,8 @@ export function CalculatorScreen({ extraClasses, value }: ScreenParam) {
         boxShadow: "0px 0px 5px 2px rgba(0,0,0,0.20) inset",
       }}
     >
-      {value}
+      <div className='col-start-1 col-end-6 row-start-1 row-end-4'>{value}</div>
+      {operationItem}
     </div>
   );
 }
